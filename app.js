bc8732325e4e20a4330f268408baa787c8767ee5
supercoolx <dm.kondratov.git@gmail.com>
2022-03-20 00:08:01 -0700

    Change var to const, create controllers and routes

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const passport = require('passport');
 require("dotenv").config();
 require('./config/passport')(passport);
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -17,7 +16,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use((req, res) => {
+    return res.status(404).json({
+        success: false
+    });
+})
 
 module.exports = app;
