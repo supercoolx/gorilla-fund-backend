be82f608d933e9d1e26e28c42cbc5c9a4b084291
supercoolx <dm.kondratov.git@gmail.com>
2022-04-18 09:04:01 -0700

    Test email

---
+++ config/mail.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer');
+
+const mail = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
+});
+
+module.exports = mail;
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,25 @@ const router = require('express').Router();
 const authRouter = require('./auth');
 const fundRouter = require('./fund');
 const userRouter = require('./user');
+const mail = require('../config/mail');
 
 router.use('/auth', authRouter);
 router.use('/fund', fundRouter);
 router.use('/user', userRouter);
+router.use('/ping', (req, res) => {
+    mail.sendMail({
+        from: process.env.MAIL_USER,
+        to: 'stepan912@dispomail.win',
+        subject: 'Sending email using node.js',
+        text: 'That was easy!'
+    })
+    .then(info => res.json(info.response))
+    .catch(err => res.send(err.message));
+});
 router.use((req, res) => {
     return res.status(404).json({
         success: false
     });
-})
+});
 
 module.exports = router;
